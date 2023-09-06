4995c630801ddc39dffc1079d60ae7547acd16e4
ThuanDH30 <thuandhde170652@fpt.edu.vn>
2023-09-06 15:00:36 +0700

    LAB

---
+++ Sorting/test/src/controller/AlgorithmController.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+/**
+ *
+ * @author Bravo
+ */
+public class AlgorithmController {
+    public static void main(String[] args) {
+        SortController run= new SortController();
+        run.run();
+    }
+    
+}
